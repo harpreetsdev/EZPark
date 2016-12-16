51c29819b7f61158e3dedd5c28156c515e1b20cf
Harpreet Singh <harpreetsdevelopers@gmail.com>
2016-12-16 17:50:43 -0500

    Initial Commit

---
+++ EZPark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EZPark
+//
+//  Created by HARPREET SINGH on 12/16/16.
+//  Copyright © 2016 HARPREET SINGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
