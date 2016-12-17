90dfaa972aaad52ec1ce46349b89aaea47c73244
Harpreet Singh <harpreetsdevelopers@gmail.com>
2016-12-17 09:15:37 -0500

    First commit

---
+++ EZPark/ServiceFactory.swift
@@ -0,0 +1,13 @@
+//
+//  ServiceFactory.swift
+//  EZPark
+//
+//  Created by HARPREET SINGH on 12/17/16.
+//  Copyright © 2016 HARPREET SINGH. All rights reserved.
+//
+
+import UIKit
+
+class ServiceFactory: NSObject {
+
+}
